bb2c981990de1a1d5e2d628e6213fd599d0c33ff
Mohmaed7070 <Mohamed.Ahmed171@compit.aun.edu.eg>
2024-10-19 13:56:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,7 @@
-// create web server
-new server();
+// Create web server
+var http = require('http');
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
+console.log('Server running on http://localhost:8080');
